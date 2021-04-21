f132545752ffd80b3f842f5aeeda7dd02a48da12
pedersonShrimp <pedersonshrimp@gmail.com>
2021-04-22 07:53:39 +1000

    feat: Add contract addresses

--- src/constants/shrimpPriceContracts.ts
+++ src/constants/shrimpPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: { shrimpAddress: string, busdAddress: string, lpAddress: string } = {
-  shrimpAddress: 'TODO',
+  shrimpAddress: '0x62ee12e4fe74a815302750913c3c796bca23e40e',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: 'TODO'
+  lpAddress: '0x0984018619DcA2B783ebf5578D9E6441C0897870'
 }
 
 export default priceContracts
